b5b02a55be484cd5a2f3afe31823b2490166f8a9
Dawsonkw <dawsonkw@gmail.com>
2024-06-08 14:53:16 -0600

    made a custom hook to debounce things. applied it to the search input

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,12 +12,14 @@ import ResultsCount from "./ResultsCount";
 import JobList from "./JobList";
 import PaginationControls from "./PaginationControls";
 import SortingControls from "./SortingControls";
-import { useJobItems } from "../lib/hooks";
+import { useDebounce, useJobItems } from "../lib/hooks";
 
 function App() {
   const [searchText, setSearchText] = useState("");
+  const debouncedSearchText = useDebounce(searchText, 200);
+
   const { jobItemsSliced, isLoading, totalNumberOfResults } =
-    useJobItems(searchText);
+    useJobItems(debouncedSearchText);
 
   return (
     <>
