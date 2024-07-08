16a156db4aa695f7c0d5f7e332fc7d58b7a3e42d
Dawsonkw <dawsonkw@gmail.com>
2024-07-08 14:09:08 -0600

    custom hook for react context provider stuff

--- src/contexts/BookmarksContextProvider.tsx
+++ src/contexts/BookmarksContextProvider.tsx
@@ -1,9 +1,14 @@
 import { createContext } from "react";
 import { useLocalStorage } from "../lib/hooks";
 
-export const BookmarksContext = createContext(null);
+type BookmarksContext = {
+  bookmarkedIds: number[];
+  handleToggleBookmark: (id: number) => void;
+};
 
-function BookmarksContextProvider({ children }) {
+export const BookmarksContext = createContext<BookmarksContext | null>(null);
+
+function BookmarksContextProvider({ children }: { children: React.ReactNode }) {
   const [bookmarkedIds, setBookmarkedIds] = useLocalStorage<number[]>(
     "bookmarkedIds",
     []
