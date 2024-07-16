c77c3f59f6bcff36e455c82c47221e8854ecb87e
Dawsonkw <dawsonkw@gmail.com>
2024-07-16 15:25:47 -0600

    I just used createPortal for the first time and it made me feel like a wizard. Out here making react magic portals and summoning typescript demons.

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,3 +1,4 @@
+import { createPortal } from "react-dom";
 import { useBookmarksContext } from "../lib/hooks";
 import JobList from "./JobList";
 import { forwardRef } from "react";
@@ -5,10 +6,11 @@ import { forwardRef } from "react";
 const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarksContext();
 
-  return (
+  return createPortal(
     <div ref={ref} className="bookmarks-popover">
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
-    </div>
+    </div>,
+    document.body
   );
 });
 
